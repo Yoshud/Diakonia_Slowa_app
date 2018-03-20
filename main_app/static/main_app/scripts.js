c71af86d285c64ef45000dbda758efc63d194414
Yoshud <adam0cisak@gmail.com>
2018-03-20 21:02:48 +0100

    Przerzucenie wielu znaczników do main_style

--- main_app/static/main_app/scripts.js
+++ main_app/static/main_app/scripts.js
@@ -1,49 +1,41 @@
 
-
-function write_main_page_product( table_ID, data ) {
-    table_ref = document.getElementById(table_ID)
-    var length_before_deleting = table_ref.rows.length;
-    var it = 0;
-    for (let i in table_ref.rows)
-    {
-        it++;
-        if(table_ref.rows.length>3) {
-            console.log("usuwane: ",length_before_deleting - it - 1, table_ref.rows.length, it+3) ;
-
-        }
-    }
-   t_delete(table_ref, 1, length_before_deleting);
-    function t_delete(table_ref, begin, end)
-    {
-        for(var it = begin; it < end; it++ )
-        {
-            table_ref.deleteRow(begin);
-        }
-    }
-
-
+function write_main_page_product(table_ID, data) {
+    table_ref = document.getElementById(table_ID);
     console.log("write: ", data.product_name);
-    function Cellstyle(newCell)
-    {
-        newCell.style.borderTopStyle = "solid";
-        newCell.style.borderLeftStyle = "solid";
-        newCell.style.borderWidth = "thin";
+    t_delete(table_ref, 1, table_ref.rows.length);
+    if (data.product_name.length < 1 && flag) {
+        alert("Brak produktu o takiej nazwie");
+        flag = false;
     }
-    function newcell(text, Row)
-    {
-        var newCell = Row.insertCell();
-        Cellstyle(newCell);
-        var newText = document.createTextNode(text);
-        newCell.appendChild(newText)
+    else {
+        for (it in data.product_name) {
+            flag = true;
+            var newRow = table_ref.insertRow();
+            newcell(data.product_name[it], newRow);
+            newcell(data.quantity[it], newRow);
+            newcell(data.price[it] + " zł", newRow);
+        }
     }
-  for (it in data.product_name)
-  {
-      console.log("size: ", table_ref.rows);
-      var newRow = table_ref.insertRow();
-      newcell(data.product_name[it], newRow);
-      newcell(data.quantity[it], newRow);
-      newcell(data.price[it] + " zł", newRow);
-  }
+    console.log("size: ", table_ref.rows);
 }
 
+function t_delete(table_ref, begin, end) {
+    for (var it = begin; it < end; it++) {
+        table_ref.deleteRow(begin);
+    }
+}
+
+function Cellstyle(newCell) {
+    newCell.style.borderTopStyle = "solid";
+    newCell.style.borderLeftStyle = "solid";
+    newCell.style.borderWidth = "thin";
+    newCell.style.paddingLeft = "5px";
+}
+
+function newcell(text, Row) {
+    var newCell = Row.insertCell();
+    Cellstyle(newCell);
+    var newText = document.createTextNode(text);
+    newCell.appendChild(newText)
+}
 
