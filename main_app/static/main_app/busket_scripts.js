118663c8f2545f9df803bf82853b1f846ec9bdaf
Yoshud <adam0cisak@gmail.com>
2018-03-22 20:25:55 +0100

    [order] Dodane usuwanie dodanych zamówień z tabeli dostępnych produktów po wyszukaniu

--- main_app/static/main_app/busket_scripts.js
+++ main_app/static/main_app/busket_scripts.js
@@ -1,4 +1,4 @@
-class busket_class {
+class basket_class {
 
     constructor() {
         this.diction = {
@@ -28,17 +28,18 @@ class busket_class {
     }
 }
 
-let busket = new busket_class();
+let basket = new basket_class();
 
-function add_product_by_id_to_busket(id, product_name, price, number_tag = "number_") {
+function add_product_by_id_to_busket(id, product_name, price, table_ID, data, number_tag = "number_") {
 
     let number_ref = $("#" + number_tag + id);
     let quantity = number_ref.val();
     console.log(id, product_name, number_ref.val(), price * quantity);
-    busket.fun();
-    if(quantity > 0) {
-        busket.add_product(product_name, id, quantity, price);
-        write_busket_table("busket_table", busket.diction, price * quantity)
+    basket.fun();
+    if (quantity > 0) {
+        basket.add_product(product_name, parseInt(id), quantity, price);
+        write_busket_table("busket_table", basket.diction, price * quantity);
+        // write_order_page_product();
     }
 }
 
@@ -51,7 +52,7 @@ function write_busket_table(table_ID, data, sum) {
         newcell(data["quantity"][it], newRow);
 
 
-        let cell = newcell((data["quantity"][it]*data["price"][it])+"zł", newRow);
+        let cell = newcell((data["quantity"][it] * data["price"][it]) + "zł", newRow);
         Cellstyle(cell);
         cell.setAttribute("id", "sum_" + data["id"][it]);
 
@@ -62,3 +63,67 @@ function write_busket_table(table_ID, data, sum) {
 
     }
 }
+
+
+function write_order_page_product(table_ID, data, restricted_id = []) {
+    table_ref = document.getElementById(table_ID);
+    t_delete(table_ref, 1, table_ref.rows.length);
+    if (data.product_name.length < 1 && flag) {
+        alert("Brak produktu o takiej nazwie");
+        flag = false;
+    }
+    else {
+        flag = true;
+        let id_it = 0;
+        for (it in data.product_name) {
+            if ((basket.diction["id"].indexOf(data.pk[it])) === -1) {
+                console.log(basket.diction["id"].indexOf(data.pk[it]), basket.diction["id"], data.pk[it]);
+                //  if (restricted_id.indexOf(data.pk[it]) == -1) {
+                let newRow = table_ref.insertRow();
+                newcell(data.product_name[it], newRow);
+                newcell(data.quantity[it], newRow);
+                let cell = newcell(data.price[it] + " zł", newRow);
+
+                cell = newRow.insertCell();
+                cell.setAttribute("onchange", "update_sum (" + data.pk[it] + "," + data.price[it] + "," + data.quantity[it] + ")");
+
+                Cellstyle(cell);
+                let input = document.createElement("INPUT");
+                input.setAttribute("type", "number");
+                input.setAttribute("id", "number_" + data.pk[it]);
+                input.setAttribute("value", 0);
+                input.setAttribute("min", 0);
+                input.setAttribute("max", data.quantity[it]);
+                input.setAttribute("required", "");
+                input.style.width = "100%";
+                input.style.height = "100%";
+                input.style.padding = 0;
+                input.style.border = 0;
+                cell.appendChild(input);
+
+                cell = newRow.insertCell();
+                Cellstyle(cell);
+                cell.setAttribute("id", "sum_" + data.pk[it]);
+                cell.innerHTML = "0zł";
+
+                cell = newRow.insertCell();
+                Cellstyle(cell);
+                cell.style.textAlign = "center";
+                cell.innerHTML = "<input type='button' value='Dodaj'>";
+                cell.dataset.pk = data.pk[it];
+                cell.dataset.product_name = data.product_name[it];
+                cell.dataset.price = data.price[it];
+                cell.dataset.data = data;
+                cell.dataset.tableID = table_ID;
+                console.log(data);
+                cell.addEventListener('click', function () {
+                    console.log(this.dataset.data);
+                    add_product_by_id_to_busket(this.dataset.pk, this.dataset.product_name, this.dataset.price, this.dataset.tableID, this.dataset.data);
+                });
+
+            }
+        }
+    }
+
+}
+
--- main_app/static/main_app/scripts.js
+++ main_app/static/main_app/scripts.js
@@ -19,65 +19,6 @@ function write_main_page_product(table_ID, data) {
     console.log("size: ", table_ref.rows);
 }
 
-function write_order_page_product(table_ID, data) {
-    table_ref = document.getElementById(table_ID);
-    t_delete(table_ref, 1, table_ref.rows.length);
-    if (data.product_name.length < 1 && flag) {
-        alert("Brak produktu o takiej nazwie");
-        flag = false;
-    }
-    else {
-        for (it in data.product_name) {
-            flag = true;
-            let newRow = table_ref.insertRow();
-            newcell(data.product_name[it], newRow);
-            newcell(data.quantity[it], newRow);
-            let cell = newcell(data.price[it] + " zł", newRow);
-
-            cell = newRow.insertCell();
-            cell.setAttribute("onchange", "update_sum ("+data.pk[it]+","+data.price[it]+","+ data.quantity[it] +")");
-
-            Cellstyle(cell);
-            let input = document.createElement("INPUT");
-            input.setAttribute("type", "number");
-            input.setAttribute("id", "number_"+data.pk[it]);
-            input.setAttribute("value", 0);
-            input.setAttribute("min", 0);
-            input.setAttribute("max", data.quantity[it]);
-            input.setAttribute("required", "");
-            input.style.width = "100%";
-            input.style.height = "100%";
-            input.style.padding = 0;
-            input.style.border = 0;
-            cell.appendChild(input);
-
-            cell = newRow.insertCell();
-            Cellstyle(cell);
-            cell.setAttribute("id", "sum_"+ data.pk[it]);
-            cell.innerHTML = "0zł";
-
-            cell = newRow.insertCell();
-            Cellstyle(cell);
-            cell.style.textAlign = "center";
-            cell.innerHTML = "<input type='button' value='Dodaj'>";
-            cell.setAttribute("onclick", "add_product_by_id_to_busket (" + data.pk[it]+ ','+ "\""+data.product_name[it]+ "\"" +","+ data.price[it] + ")");
-        }
-    }
-}
-/*
-function fun(fun_name, fun_attributs){
-    var ret = fun_name + "(";
-    for(it in fun_attributs){
-        if (it===0)
-            ret += fun_attributs[it];
-        else
-        {
-
-        }
-
-    }
-}
-*/
 function validate_number_count(number_tag, id, max_quantity){
     let number_ref = $("#"+number_tag +id);
     let how_many = number_ref.val();
