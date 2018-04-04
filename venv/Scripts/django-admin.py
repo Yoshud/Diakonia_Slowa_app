9fc73046aba9c1bc2ff2bb7d6531c018120a3704
Yoshud <adam0cisak@gmail.com>
2018-04-04 20:48:57 +0200

    Dodanie virtualenv

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\Notatki\programowanie\Projekty\Python\Diakonia_Slowa\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
