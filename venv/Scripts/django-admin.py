8fb631204fdcf48c7a7aaa35c5ae20fa3939245f
Yoshud <adam0cisak@gmail.com>
2018-04-09 23:34:17 +0200

    remote

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!E:\Notatki\programowanie\Projekty\Python\Diakonia_Slowa\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
